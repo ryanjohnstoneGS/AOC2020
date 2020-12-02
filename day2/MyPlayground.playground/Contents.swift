7881bb8a7b2aca67e2b6d52f82da626cc12321e9
Ryan Johnstone <r.johnstone@gymshark.com>
2020-12-02 15:16:23 +0000

    day 2 initial solution

--- day2/MyPlayground.playground/Contents.swift
+++ day2/MyPlayground.playground/Contents.swift
@@ -1,3 +1,38 @@
 import Cocoa
 
-var str = "Hello, playground"
+let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
+let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+
+var validCount = 0
+for line in content.components(separatedBy: "\n") {
+    print(line)
+    if line.count == 0 { break }
+
+    let spaceSeperated = line.components(separatedBy: " ")
+
+    let range = spaceSeperated.first!
+    let password = spaceSeperated.last!
+    let character = spaceSeperated[1].first!
+
+    let min = Int(range.components(separatedBy: "-").first!)!
+    let max = Int(range.components(separatedBy: "-")[1])!
+
+    if min > 0 {
+        if !password.contains(character) {
+            continue
+        }
+
+        var occurenceCount = 0
+        for char in password {
+            if char == character {
+                occurenceCount += 1
+            }
+        }
+
+        if occurenceCount >= min && occurenceCount <= max {
+            validCount += 1
+        }
+    }
+}
+
+print(validCount)
