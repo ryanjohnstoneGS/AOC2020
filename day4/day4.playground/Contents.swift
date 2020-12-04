f4488a380a6cbc7cddf78c7b097b42798e6ac462
Ryan Johnstone <r.johnstone@gymshark.com>
2020-12-04 15:20:31 +0000

    part 2

--- day4/day4.playground/Contents.swift
+++ day4/day4.playground/Contents.swift
@@ -5,8 +5,6 @@ let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
 
 let lines = content.components(separatedBy: "\n")
 
-//print(lines)
-
 let validKeys = Set(["byr", "iyr", "eyr", "hgt","hcl","ecl","pid"])
 
 var id: [String: String] = [:]
@@ -15,12 +13,12 @@ for line in lines {
     let pairs = line.components(separatedBy: " ")
     for pair in pairs {
         let parts = pair.components(separatedBy: ":")
-       // print(parts)
 
         if parts[0].count == 0 {
-          //  print(Set(id.keys))
             if Set(id.keys) == validKeys {
-                validCount += 1
+                if validValues(id) {
+                    validCount += 1
+                }
             }
             id = [:]
             continue
@@ -33,9 +31,69 @@ for line in lines {
 
     if line == lines.last {
         if Set(id.keys) == validKeys {
-            validCount += 1
+            if validValues(id) {
+                validCount += 1
+            }
         }
     }
 }
 
+func validValues(_ id: [String:String]) -> Bool {
+    for (key,value) in id {
+        switch PassportKeys(rawValue: key) {
+        case .byr:
+            if value.doesNotMatch("(19[2-9][0-9])|(200[0-2])") {
+                return false
+            }
+        case .iyr:
+            if value.doesNotMatch("201[0-9]|2020") {
+                return false
+            }
+        case .eyr:
+            if value.doesNotMatch("202[0-9]|2030") {
+                return false
+            }
+        case .hgt:
+            if value.doesNotMatch("(1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in") {
+                return false
+            }
+        case .hcl:
+            if value.doesNotMatch("#[0-9a-f]{6}") {
+                return false
+            }
+        case .ecl:
+            if value.doesNotMatch("(amb|blu|brn|gry|grn|hzl|oth)") {
+                return false
+            }
+        case .pid:
+            if value.doesNotMatch("^[0-9]{9}$") {
+                return false
+            }else {
+                print(value)
+            }
+        case .none: break
+
+        }
+    }
+
+    return true
+}
+
+enum PassportKeys: String {
+    case byr
+    case iyr
+    case eyr
+    case hgt
+    case hcl
+    case ecl
+    case pid
+}
+
+extension String {
+    func doesNotMatch(_ string: String) -> Bool {
+        let regex = try! NSRegularExpression(pattern: string)
+        let range = NSRange(location: 0, length: self.utf16.count)
+        return regex.firstMatch(in: self, options: [], range: range) == nil
+    }
+}
 print(validCount)
